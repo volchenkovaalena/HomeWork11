a4ad008a45b2d80d632dac9753131478ce09d8e7
alena <misamistake@>
2024-01-16 22:41:20 +0300

    Did HW2

--- src/Main.java
+++ src/Main.java
@@ -14,21 +14,22 @@ public class Main {
             System.out.println ("Установите облегченную версию приложения для Android по ссылке");
         }
     }
-    public static void printDeliveryTime (int distance) {
-        int deliveryTime = 1;
-        if (distance < 20) {System.out.println ("Потребуется дней " + deliveryTime);}
-        if (distance >= 20 && distance < 60 ) {
-            deliveryTime = deliveryTime + 1;
-            System.out.println ("Потребуется дней " +deliveryTime);}
-        if (distance >= 60 && distance<= 100) {
-            deliveryTime = deliveryTime + 2;
-            System.out.println ("Потребуется дней " + deliveryTime);}
-        if (distance > 100 ) {
-            System.out.println (" Доставка в ваш район не осущствляется");
+    public static int deliveryTime (int deliveryDistance)  {
+        {
+            if (deliveryDistance <= 20) {
+                return 1;
+            } else if (deliveryDistance <= 60) {
+                return 2;
+            } else if (deliveryDistance <= 100) {
+                return 3;
+            } else {
+                return -1;
+            }
         }
-    }
-    public static void main ( String[] args) {
 
+        }
+
+    public static void main ( String[] args) {
         int year = 2024;
         printYear ( year );
 
@@ -36,7 +37,14 @@ public class Main {
         int deviseYear = 2020;
         printOS (clientOS, deviseYear);
 
-        int deliveryDistance = 56;
-        printDeliveryTime ( deliveryDistance );
+
+        int deliveryDistance = 77;
+        int days = deliveryTime (deliveryDistance);
+        if (days > 0 ) {
+            System.out.println ("Доставка составит " + days + " дня" );}
+            else { System.out.println ("Доставка не осуществляется" );}
+
+
+
     }
 }
